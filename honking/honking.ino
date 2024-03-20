2fde38f5d733f17532be9732d7dd111652471c52
K-Ganesh-Rao <130823089+K-Ganesh-Rao@users.noreply.github.com>
2024-03-20 15:15:36 +0530

    Update honking.ino
    Uart to I2c

--- honking/honking.ino
+++ honking/honking.ino
@@ -1,43 +1,31 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-const int button_pin = 6;
-unsigned long buttonPressedAt = 0;
-unsigned long buttonReleasedAt = 0;
-unsigned long duration = 0;
-unsigned long cumulativeTime = 0;
-bool buttonState = HIGH; 
-unsigned long pressCount = 0;
+// set the LCD number of columns and rows
+int lcdColumns = 16;
+int lcdRows = 2;
 
-void setup() {
-  lcd.begin(16, 2);
-  pinMode(button_pin, INPUT);
-  Serial.begin(9600);
+// set LCD address, number of columns and rows
+// if you don't know your display address, run an I2C scanner sketch
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+
+void setup(){
+  // initialize LCD
+ // lcd.init();
+  // turn on LCD backlight                      
+  lcd.backlight();
 }
 
-void loop() {
-  int state = digitalRead(button_pin);
-
-  if (state == HIGH && buttonState == LOW) {
-    buttonPressedAt = millis();
-    buttonState = HIGH; 
-  } else if (state == LOW && buttonState == HIGH) {
-    buttonReleasedAt = millis();
-    duration = buttonReleasedAt - buttonPressedAt;
-    cumulativeTime += duration;
-    buttonState = LOW; 
-    Serial.print(cumulativeTime);
-    Serial.println(" ms");
-    pressCount++;
-
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Honking: ");
-    lcd.print(pressCount/2);
-    delay(100);
-    lcd.setCursor(0, 1);
-    lcd.print("cum_dur: ");
-    lcd.print(cumulativeTime/1000);
-  }
+void loop(){
+  // set cursor to first column, first row
+  lcd.setCursor(0, 0);
+  // print message
+  lcd.print("Hello, World!");
+  delay(1000);
+  // clears the display to print new message
+  lcd.clear();
+  // set cursor to first column, second row
+  lcd.setCursor(0,1);
+  lcd.print("Hello, World!");
+  delay(1000);
+  lcd.clear(); 
 }
