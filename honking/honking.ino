b1669ca8422a8cdd5e9651880dc2e0a8adb85402
CderSkan <110719470+CderSkan@users.noreply.github.com>
2024-03-20 15:56:05 +0530

    esp.ino
    just for voltage method

--- honking/honking.ino
+++ honking/honking.ino
@@ -1,31 +1,46 @@
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// set the LCD number of columns and rows
-int lcdColumns = 16;
-int lcdRows = 2;
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Set the LCD address to 0x27 for a 16 chars and 2 line display
 
-// set LCD address, number of columns and rows
-// if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+const int buttonPin = D0;  // Pin connected to the button
+unsigned long lastPressTime = 0;
+int buttonState = 0;
+int lastButtonState = 0;
+int buttonPressCount = 0;
+unsigned long totalPressedTime = 0;
 
-void setup(){
-  // initialize LCD
- // lcd.init();
-  // turn on LCD backlight                      
+void setup() {
+  Serial.begin(9600);
+ 
   lcd.backlight();
+  pinMode(buttonPin, INPUT);
+  lcd.print("Button Count:");
 }
 
-void loop(){
-  // set cursor to first column, first row
-  lcd.setCursor(0, 0);
-  // print message
-  lcd.print("Hello, World!");
-  delay(1000);
-  // clears the display to print new message
-  lcd.clear();
-  // set cursor to first column, second row
-  lcd.setCursor(0,1);
-  lcd.print("Hello, World!");
-  delay(1000);
-  lcd.clear(); 
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState != lastButtonState) {
+    if (buttonState == HIGH) {
+      lastPressTime = millis();
+    } else {
+      totalPressedTime += millis() - lastPressTime;
+      buttonPressCount++;
+      Serial.print("Button Pressed ");
+      Serial.print(buttonPressCount);
+      Serial.print(" times, Total Pressed Time: ");
+      Serial.print(totalPressedTime / 1000); // Convert milliseconds to seconds
+      Serial.println(" seconds");
+
+      lcd.setCursor(0, 1);
+      lcd.print("Press: ");
+      lcd.print(buttonPressCount);
+      lcd.print(", Time: ");
+      lcd.print(totalPressedTime / 1000); // Convert milliseconds to seconds
+      lcd.print("s");
+    }
+  }
+
+  lastButtonState = buttonState;
 }
