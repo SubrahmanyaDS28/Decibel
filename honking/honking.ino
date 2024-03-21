6791d3a30b5ebc8e27455cacb747543b01e71c4a
lawliet63 <112820170+lawliet63@users.noreply.github.com>
2024-03-21 21:27:09 +0530

    Update honking.ino

--- honking/honking.ino
+++ honking/honking.ino
@@ -2,82 +2,38 @@
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // Set the LCD address to 0x27 for a 16 chars and 2 line display
-
-const int buttonPin = D0;  // Pin connected to the button
+int analogSensorCount = 0;  // Pin connected to the button
 const int analogPin = A0;   // Pin connected to the analog sensor
-unsigned long lastPressTime = 0;
-int buttonState = 0;
-int lastButtonState = 0;
-int buttonPressCount = 0;
-unsigned long totalPressedTime = 0;
-int analogSensorCount = 0;
-unsigned long aboveThresholdDuration = 0;
-unsigned long lastAboveThresholdTime = 0;
 const int threshold = 465;
-int flag = 1;
+unsigned long honk_start = 0;
+unsigned long honk_dur = 0;
+unsigned long total_dur = 0;
+
+
 
 void setup() {
   Serial.begin(9600);
- 
   lcd.backlight();
-  pinMode(buttonPin, INPUT);
-  lcd.print("Button Count:");
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-
-  if (buttonState != lastButtonState) {
-    if (buttonState == HIGH) {
-      lastPressTime = millis();
-    } else {
-      totalPressedTime += millis() - lastPressTime;
-      buttonPressCount++;
-      Serial.print("Button Pressed ");
-      Serial.print(buttonPressCount);
-      Serial.print(" times, Total Pressed Time: ");
-      Serial.print(totalPressedTime / 1000); // Convert milliseconds to seconds
-      Serial.println(" seconds");
-
-      lcd.setCursor(0, 1);
-      lcd.print("Press: ");
-      lcd.print(buttonPressCount);
-      lcd.print(", Time: ");
-      lcd.print(totalPressedTime / 1000); // Convert milliseconds to seconds
-      lcd.print("s");
-    }
-  }
-
-  lastButtonState = buttonState;
-
-  // Analog sensor reading and processing
   int sensorValue = analogRead(analogPin);
-  if (sensorValue < threshold && flag =1) {
-    if (analogSensorCount == 0) {
-      lastAboveThresholdTime = millis();
-    }
-    if(flag = 1)
-    {
-      analogSensorCount++;
-      flag =0;
-    aboveThresholdDuration = millis() - lastAboveThresholdTime;
-    Serial.print("Analog Sensor Count: ");
-    Serial.print(analogSensorCount);
-    Serial.print(", Above Threshold Duration: ");
-    Serial.print(aboveThresholdDuration / 1000); // Convert milliseconds to seconds
-    Serial.println(" seconds");
-
+  if (sensorValue < threshold) {
+        honk_start = millis();
+        while(sensorValue < threshold){
+          sensorValue = analogRead(analogPin);
+          if(sensorValue > threshold){
+            analogSensorCount = analogSensorCount +1;
+            honk_dur = millis() - honk_start;
+            break;
+          }
+        }
+  }
+    total_dur = total_dur + honk_dur;
     lcd.setCursor(0, 2);
     lcd.print("Sensor Count: ");
     lcd.print(analogSensorCount);
     lcd.print(", Duration: ");
-    lcd.print(aboveThresholdDuration / 1000); // Convert milliseconds to seconds
+    lcd.print(total_dur / 1000); // Convert milliseconds to seconds
     lcd.print("s");
-    }
-    
-  }
-  else if(sensorValue > threshold)
-  {
-    flag = 1;
-  }
-}
+    } 
